5a821f1d5611b056440221159b5bf3d67db6cc5b
Muhammad Zohaib <zohaib10092001@gmail.com>
2023-01-13 17:22:24 +0500

    layouts added to root page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { Provider } from 'jotai'
-import dynamic from 'next/dynamic'
-import Sidebar from '../components/layouts/Sidebar'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Provider } from "jotai";
+import dynamic from "next/dynamic";
+import Sidebar from "../components/layouts/Sidebar";
+import Layout from "../components/layouts/Layout";
 // import Header from '../components/layouts/Header'
 // import Footer from '../components/layouts/Footer'
-const Header = dynamic(() => import('../components/layouts/Header'), {
+// const Header = dynamic(() => import('../components/layouts/Header'), {
+//   ssr: false,
+// })
+// const Footer = dynamic(() => import('../components/layouts/Footer'), {
+//   ssr: false,
+// })
+const Footer = dynamic(() => import("../components/layouts/Layout"), {
   ssr: false,
-})
-const Footer = dynamic(() => import('../components/layouts/Footer'), {
-  ssr: false,
-})
+});
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    < >
-    <Provider>
-      <Header/>
-      <Sidebar/>
-      <Component {...pageProps} />
-      <Footer/>
-    </Provider>
-    
+    <>
+      <Provider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
     </>
-  )
+  );
 }

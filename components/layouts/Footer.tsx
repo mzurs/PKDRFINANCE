b39d3b366345c5c65640c72de26b58843d3a4198
Muhammad Zohaib <zohaib10092001@gmail.com>
2023-01-14 01:42:47 +0500

    temp footer component added

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -1,23 +1,39 @@
+import Link from "next/link";
+
 const Footer = () => {
-    return (
-      <footer className="fixed bottom-0 left-0 z-50 w-full h-16 bg-gray-900 text-white  ">
-        <div className="container mx-auto flex items-center justify-between px-4 h-full">
-          <p className="text-sm">Copyright © PKDR Finance</p>
-          <div className="flex">
-            <a href="#" className="px-3 py-2 rounded-full text-gray-500 hover:text-white">
-              <i className="fab fa-facebook fa-lg"></i>
-            </a>
-            <a href="#" className="px-3 py-2 rounded-full text-gray-500 hover:text-white">
-              <i className="fab fa-twitter fa-lg"></i>
-            </a>
-            <a href="#" className="px-3 py-2 rounded-full text-gray-500 hover:text-white">
-              <i className="fab fa-instagram fa-lg"></i>
-            </a>
-          </div>
-        </div>
-      </footer>
-    );
-  };
-  
-  export default Footer;
-  
+  return (
+    <footer className="fixed bottom-0 left-0 z-20 w-full p-4 bg-black border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+      <span className="text-sm text-white sm:text-center dark:text-gray-400">
+        © 2023{" "}
+        <a href="https://flowbite.com/" className="hover:underline">
+          PKDR Finance™
+        </a>
+        . All Rights Reserved.
+      </span>
+      <ul className="flex flex-wrap items-center mt-3 text-sm text-white dark:text-gray-400 sm:mt-0">
+        <li>
+          <a href="#" className="mr-4 hover:underline md:mr-6 ">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#" className="mr-4 hover:underline md:mr-6">
+            Privacy Policy
+          </a>
+        </li>
+        <li>
+          <a href="#" className="mr-4 hover:underline md:mr-6">
+            Licensing
+          </a>
+        </li>
+        <li>
+          <a href="#" className="hover:underline">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;

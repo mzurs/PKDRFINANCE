fe89d7d990ed9f05894b1b07d2b050a4e0aaf8b2
Muhammad Zohaib <zohaib10092001@gmail.com>
2023-01-13 17:25:06 +0500

    index page reinitialized

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,19 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import dynamic from 'next/dynamic'
+import type { NextPage } from "next";
+import { privKeyAtom } from "../state/jotai";
+import { useEffect } from "react";
+import { useAtom, useAtomValue } from "jotai";
 // const Loading = dynamic(() => import('../components/loading/Loading'), {
-  // ssr: false,
+// ssr: false,
 // })
-const Home: NextPage = () => {
-  
-  return (
-    <div>Index</div>
-  )
-}
 
-export default Home
+const Home: NextPage = () => {
+  const [privKey, setPrivKey] = useAtom(privKeyAtom);
+const privKeyValue=useAtomValue(privKeyAtom)
+  useEffect(()=>{
+    if(privKeyValue){
+      setPrivKey(privKeyValue)
+    }
+  })
+  return <div>{privKey? JSON.stringify(privKey):""}</div>;
+};
+export default Home;

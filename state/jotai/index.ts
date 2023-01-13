09843adb1b24025b2998da09338938fcb1163e37
Muhammad Zohaib <zohaib10092001@gmail.com>
2023-01-13 17:28:52 +0500

    jotai state management added with Provider on _app.tsx

--- state/jotai/index.ts
+++ state/jotai/index.ts
@@ -1,10 +1,14 @@
-import { atom, useAtom } from "jotai";
-// import {
-//   atomWithStorage,
-//   createJSONStorage,
-// } from "jotai/utils";
+import { SafeEventEmitterProvider } from "@web3auth/base";
+import { Web3AuthCore } from "@web3auth/core";
+import { atom } from "jotai";
+import { atomWithStorage, createJSONStorage } from "jotai/utils";
 
-// // const storage = createJSONStorage(() => localStorage);
-// // const count = atomWithStorage("count", 0, storage);
-// const count=0
-// export { count };
+const storage = { ...createJSONStorage(() => localStorage), delayInit: true };
+const web3authAtom = atomWithStorage("web3auth", null, storage);
+const web3authStateAtom = atom<Web3AuthCore | null>(null);
+const providerAtom=atom<SafeEventEmitterProvider|null|unknown>(null)
+const privKeyAtom = atomWithStorage("privKey", null, storage);
+const userInfoAtom = atomWithStorage("userInfo", null, storage);
+const auth = atom<Web3AuthCore | null>(null);
+
+export { web3authAtom, auth, privKeyAtom, userInfoAtom, web3authStateAtom,providerAtom };

61fbf4e4a918373417bdb97f102cd2b7f05d32ad
Muhammad Zohaib <zohaib10092001@gmail.com>
2023-01-14 20:52:06 +0500

    front component for index page is added

---
+++ components/Front.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { web3authAtom } from '../state/jotai';
+import { useAtomValue } from 'jotai';
+import Image from 'next/image';
+function Front() {
+    const auth=useAtomValue(web3authAtom);
+    return (
+     <div> PKDR FINANCE</div> 
+       );
+}
+
+export default Front

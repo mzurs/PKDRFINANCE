daa2677a1914dacf01b52c73de67735587294ce2
Muhammad Zohaib <zohaib10092001@gmail.com>
2023-01-15 17:31:17 +0500

    minor change

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,8 +2,12 @@ import React, { useState } from "react";
 import Loading from "../components/loading/Loading";
 
 function login() {
- 
-  return <div> <Loading state={true}/></div>
+  return (
+    <div>
+      {" "}
+      <Loading state={true} />
+    </div>
+  );
 }
 
 export default login;
--- state/jotai/index.ts
+++ state/jotai/index.ts
@@ -2,20 +2,28 @@ import { SafeEventEmitterProvider } from "@web3auth/base";
 import { Web3AuthCore } from "@web3auth/core";
 import { atom } from "jotai";
 import { atomWithStorage, createJSONStorage } from "jotai/utils";
+
 //storage object for local storage
 const storage = { ...createJSONStorage(() => localStorage), delayInit: true };
+
 // id Token
 const web3authAtom = atomWithStorage("web3auth", null, storage);
+
 // web3auth initialized state
 const web3authStateAtom = atom<Web3AuthCore | null>(null);
+
 //proider initialzed state
 const providerAtom = atom<SafeEventEmitterProvider | null | unknown>(null);
+
 // privateKey
 const privKeyAtom = atomWithStorage("privKey", null, storage);
+
 //user info
 const userInfoAtom = atomWithStorage("userInfo", null, storage);
+
 //simple auth for web3auth
 const auth = atom<Web3AuthCore | null>(null);
+
 // loading state for loading component
 const loading = atom(true);
 

5fb918d64af5a8835797ec9219a580dd6823e330
Muhammad Zohaib <zohaib10092001@gmail.com>
2023-01-18 17:52:01 +0500

    middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,37 @@
-// middleware.ts
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import isAuthenticated from "./lib/auth/auth";
+import userIdenity from "./lib/users";
+import { web3AuthVerifyResult } from "./lib/types/types";
 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-    console.log(JSON.stringify(request))
-  return NextResponse.redirect(new URL("/about", request.url));
+export default async function middleware(request: NextRequest) {
+  // if (request.cookies.get("web3auth") && request.cookies.get("pub_key")) {
+  //   const isAuthenticatedValue: web3AuthVerifyResult = await isAuthenticated(
+  //     request
+  //   );
+  //   if (isAuthenticatedValue.result) {
+  //     console.log(`isAuthenticatedValue: ${isAuthenticatedValue}`);
+
+  //     const userPath = await userIdenity(isAuthenticatedValue.decodedJWT);
+  //     if (!userPath) {
+  //       return NextResponse.redirect(new URL("/", request.url));
+  //     }
+  //     if (userPath === "users") {
+  //       request.cookies.set("userRole", userPath);
+  //       console.log(`Path Name from users: ${request.nextUrl.pathname}`)
+  //       if (
+  //         request.nextUrl.pathname==="/"
+  //       ) {
+  //         return NextResponse.redirect(new URL("/user/users/", request.url));
+  //       }
+  //     }
+  //     return NextResponse.next();
+  //   } else {
+  //     return NextResponse.redirect(new URL("/", request.url));
+  //   }
+  // } else {
+  //   return NextResponse.redirect(new URL("/", request.url));
+  // }
 }
-
 export const config = {
-  matcher: "/login/:path*",
+  // matcher: ["/login/:path*", "/profile/:path*", "/api/:path*", ],
 };

32f50a53bb0ad5b18b282a86c3843eb38a7920fc
Muhammad Zohaib <zohaib10092001@gmail.com>
2023-01-19 05:45:30 +0500

    role function with oAuthIdToken decoding added for role identification

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -1,22 +1,35 @@
-
 import * as jose from "jose";
-
+import * as jwt from "jsonwebtoken";
 
 type response = {
   userRole: string | null;
   result: boolean;
 };
 
-async function role(
-  params: jose.JWTVerifyResult & jose.ResolvedKey
-): Promise<string> {
+async function role(oAuthIdToken: string): Promise<string | null> {
+  const decoded: any = jwt.decode(oAuthIdToken);
+  // console.log(`Decoded: ${JSON.stringify(decoded)}`);
+  const groups = decoded["cognito:groups"];
+  // const userRole: string | null = groups.includes("Admin");
+  if (groups.includes("Admin")) {
+    return "admin";
+  } else if (groups.includes("us-west-2_cPjOesJgg_Google")) {
+    return "users";
+  } else {
+    return null;
+  }
+  // console.log("🚀 ~ file: auth.ts:14 ~ role ~ userRole", userRole);
+  // console.log("🚀 ~ file: auth.ts:13 ~ role ~ groups", groups)
+
   return "users";
 }
 export default async function auth(
   idToken: string,
-  pub_key: string
+  pub_key: string,
+  oAuthIdToken: string
 ): Promise<response> {
-  console.log("🚀 ~ file: auth.ts:21 ~ pub_key", pub_key);
+  console.log("🚀 ~ file: auth.ts:19 ~ oAuthIdToken", oAuthIdToken);
+  // console.log("🚀 ~ file: auth.ts:21 ~ pub_key", pub_key);
   // console.log("🚀 ~ file: auth.ts:21 ~ idToken", idToken)
   const res: response = {
     userRole: null,
@@ -40,7 +53,7 @@ export default async function auth(
     if (wallet === pub_key) {
       res.result = true;
 
-      const userRole = await role(jwtDecoded);
+      const userRole = await role(oAuthIdToken);
 
       if (userRole) {
         res.userRole = userRole;

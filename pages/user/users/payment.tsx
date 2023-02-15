87e27f37966fdab00dc57a35069011ffbf44636c
Syed Ammar <syed.ammar.200226.alam@gmail.com>
2023-02-16 00:41:45 +0500

    Stripe added in contacts page

--- pages/user/users/payment.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const payment = () => {
-  return (
-    <div className='py-24 text-2xl'>Payment Now</div>
-  )
-}
-
-export default payment
--- pages/user/users/user_contacts.tsx
+++ pages/user/users/user_contacts.tsx
@@ -20,91 +20,120 @@ function contacts() {
 
   return (
     <>
-      <section className="text-gray-600 body-font">
-        <div className="container px-2 py-24 mx-auto overflow-x-hidden">
-          <div className="flex items-center justify-between lg:w-2/5 mx-auto border-b pb-4 mb-10 border-gray-200">
-            <input
-              type="text"
-              name="search"
-              id="search"
-              placeholder="Search Contacts"
-              className="md:w-full focus:outline-none"
-            />
-            <button className="text-2xl">
-              <FiSearch className="hover:text-[#0389b1]" title="Search"/>
-            </button>
-          </div>
-
-          <div className="flex items-center lg:w-3/5 mx-auto border-b pb-4 mb-10 border-gray-200 sm:flex-row flex-col">
-            <div className="rounded-full p-2 bg-[#0ab0e3] mx-2">
-              <GoPerson className="text-3xl text-gray-100" />
+      <form action="/api/checkout/checkout_sessions" method="POST">
+        <div className=" text-gray-600 body-font">
+          <div className="w-[50%] px-2 mx-auto py-20">
+            <div className="mx-auto flex items-center justify-between lg:w-[82%] border-b pb-4 mb-10 border-gray-200">
+              <input
+                type="text"
+                name="search"
+                id="search"
+                placeholder="Search Contacts"
+                className="focus:outline-none text-md"
+              />
+              <button className="text-2xl">
+                <FiSearch className="hover:text-[#0389b1]" title="Search" />
+              </button>
             </div>
-            <div className="flex-grow flex items-center justify-between  sm:text-left text-center mt-6 sm:mt-0 ">
-              <div className="mx-4 float-left">
-                <h2 className="text-gray-900 text-lg title-font font-medium mb-1">
-                  Zohaib
-                </h2>
-                <p>example@gmail.com</p>
+
+            <div className="flex items-center mx-auto lg:w-[95%] border-b pb-4 mb-10 border-gray-200 sm:flex-row flex-col">
+              <div className="rounded-full p-2 bg-[#0ab0e3] mx-2">
+                <GoPerson className="text-3xl text-gray-100" />
               </div>
-              <Link href="/user/users/payment" className="text-md p-2 text-[#009ac9] hover:underline">
-                Pay Now
-              </Link>
-            </div>
-          </div>
-
-          <div className="flex items-center lg:w-3/5 mx-auto border-b pb-4 mb-10 border-gray-200 sm:flex-row flex-col">
-            <div className="rounded-full p-2 bg-[#0ab0e3] mx-2">
-              <GoPerson className="text-3xl text-gray-100" />
-            </div>
-            <div className="flex-grow flex items-center justify-between  sm:text-left text-center mt-6 sm:mt-0 ">
-              <div className="mx-4 float-left">
-                <h2 className="text-gray-900 text-lg title-font font-medium mb-1">
-                  Zohaib
-                </h2>
-                <p>example@gmail.com</p>
+              <div className="flex-grow flex items-center justify-between  sm:text-left text-center mt-6 sm:mt-0 text-sm">
+                <div className="mx-4 float-left">
+                  <h2 className="text-gray-900 text-md title-font font-medium mb-1">
+                    Zohaib
+                  </h2>
+                  <p>example@gmail.com</p>
+                </div>
+                <button
+                  className="text-md p-2 text-[#009ac9] hover:underline"
+                  type="submit"
+                >
+                  Pay Now
+                </button>
               </div>
-              <Link href="/user/users/payment" className="text-md p-2 text-[#009ac9] hover:underline">
-                Pay Now
-              </Link>
             </div>
-          </div>
-
-          <div className="flex items-center lg:w-3/5 mx-auto border-b pb-4 mb-10 border-gray-200 sm:flex-row flex-col">
-            <div className="rounded-full p-2 bg-[#0ab0e3] mx-2">
-              <GoPerson className="text-3xl text-gray-100" />
-            </div>
-            <div className="flex-grow flex items-center justify-between  sm:text-left text-center mt-6 sm:mt-0 ">
-              <div className="mx-4 float-left">
-                <h2 className="text-gray-900 text-lg title-font font-medium mb-1">
-                  Zohaib
-                </h2>
-                <p>example@gmail.com</p>
+            <div className="flex items-center mx-auto lg:w-[95%] border-b pb-4 mb-10 border-gray-200 sm:flex-row flex-col">
+              <div className="rounded-full p-2 bg-[#0ab0e3] mx-2">
+                <GoPerson className="text-3xl text-gray-100" />
               </div>
-              <Link href="/user/users/payment" className="text-md p-2 text-[#009ac9] hover:underline">
-                Pay Now
-              </Link>
-            </div>
-          </div>
-
-          <div className="flex items-center lg:w-3/5 mx-auto border-b pb-4 mb-10 border-gray-200 sm:flex-row flex-col">
-            <div className="rounded-full p-2 bg-[#0ab0e3] mx-2">
-              <GoPerson className="text-3xl text-gray-100" />
-            </div>
-            <div className="flex-grow flex items-center justify-between  sm:text-left text-center mt-6 sm:mt-0 ">
-              <div className="mx-4 float-left">
-                <h2 className="text-gray-900 text-lg title-font font-medium mb-1">
-                  Zohaib
-                </h2>
-                <p>example@gmail.com</p>
+              <div className="flex-grow flex items-center justify-between  sm:text-left text-center mt-6 sm:mt-0 text-sm">
+                <div className="mx-4 float-left">
+                  <h2 className="text-gray-900 text-md title-font font-medium mb-1">
+                    Zohaib
+                  </h2>
+                  <p>example@gmail.com</p>
+                </div>
+                <button
+                  className="text-md p-2 text-[#009ac9] hover:underline"
+                  type="submit"
+                >
+                  Pay Now
+                </button>
               </div>
-              <Link href="/user/users/payment" className="text-md p-2 text-[#009ac9] hover:underline">
-                Pay Now
-              </Link>
+            </div>
+            <div className="flex items-center mx-auto lg:w-[95%] border-b pb-4 mb-10 border-gray-200 sm:flex-row flex-col">
+              <div className="rounded-full p-2 bg-[#0ab0e3] mx-2">
+                <GoPerson className="text-3xl text-gray-100" />
+              </div>
+              <div className="flex-grow flex items-center justify-between  sm:text-left text-center mt-6 sm:mt-0 text-sm">
+                <div className="mx-4 float-left">
+                  <h2 className="text-gray-900 text-md title-font font-medium mb-1">
+                    Zohaib
+                  </h2>
+                  <p>example@gmail.com</p>
+                </div>
+                <button
+                  className="text-md p-2 text-[#009ac9] hover:underline"
+                  type="submit"
+                >
+                  Pay Now
+                </button>
+              </div>
+            </div>
+            <div className="flex items-center mx-auto lg:w-[95%] border-b pb-4 mb-10 border-gray-200 sm:flex-row flex-col">
+              <div className="rounded-full p-2 bg-[#0ab0e3] mx-2">
+                <GoPerson className="text-3xl text-gray-100" />
+              </div>
+              <div className="flex-grow flex items-center justify-between  sm:text-left text-center mt-6 sm:mt-0 text-sm">
+                <div className="mx-4 float-left">
+                  <h2 className="text-gray-900 text-md title-font font-medium mb-1">
+                    Zohaib
+                  </h2>
+                  <p>example@gmail.com</p>
+                </div>
+                <button
+                  className="text-md p-2 text-[#009ac9] hover:underline"
+                  type="submit"
+                >
+                  Pay Now
+                </button>
+              </div>
+            </div>
+            <div className="flex items-center mx-auto lg:w-[95%] border-b pb-4 mb-10 border-gray-200 sm:flex-row flex-col">
+              <div className="rounded-full p-2 bg-[#0ab0e3] mx-2">
+                <GoPerson className="text-3xl text-gray-100" />
+              </div>
+              <div className="flex-grow flex items-center justify-between  sm:text-left text-center mt-6 sm:mt-0 text-sm">
+                <div className="mx-4 float-left">
+                  <h2 className="text-gray-900 text-md title-font font-medium mb-1">
+                    Zohaib
+                  </h2>
+                  <p>example@gmail.com</p>
+                </div>
+                <button
+                  className="text-md p-2 text-[#009ac9] hover:underline"
+                  type="submit"
+                >
+                  Pay Now
+                </button>
+              </div>
             </div>
           </div>
-
         </div>
-      </section>
+      </form>
     </>
   );
 }

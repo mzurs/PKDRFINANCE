fdaae3d6aacf2a6a1f70c58af443adc317ac2ae5
Syed Ammar <syed.ammar.200226.alam@gmail.com>
2023-03-31 01:32:50 +0500

    setUserName API Integrated

--- components/users/settingsLayout/ProfileInfo.tsx
+++ components/users/settingsLayout/ProfileInfo.tsx
@@ -1,10 +1,42 @@
-import React from "react";
-import { useAtomValue } from "jotai";
-import { userInfoAtom } from "../../../state/jotai";
+import React, { useState } from "react";
+import { useAtom, useAtomValue } from "jotai";
+import { userInfoAtom, web3authAtom } from "../../../state/jotai";
 import { UserInfo } from "./type/userTypes";
 
 const ProfileInfo = () => {
   const info: UserInfo = useAtomValue(userInfoAtom);
+  const [data, setData] = useState<{
+    userName: string;
+  }>({
+    userName: "CHECK",
+  });
+
+  const handleSubmit = async () => {
+    console.log(`Id Token : ${info.idToken}`);
+    const formData = {
+      data,
+    };
+    const headers = new Headers();
+    headers.append("content-type", "application/json");
+    headers.append(
+      "x-custom-header",
+      JSON.stringify([info.idToken, info.oAuthIdToken])
+    );
+    await fetch("/api/mutation/setUserName", {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then(async (data) => {
+        
+        if (data) {
+          console.log(`RESPONSE: ${Object.values(data)}`
+          );
+        }
+      });
+  };
+
   return (
     <div>
       <div className="pl-4">
@@ -27,7 +59,10 @@ const ProfileInfo = () => {
                   </dt>
                   <dd className="mt-1 text-md md:text-lg text-gray-900 sm:col-span-2 sm:mt-0">
                     {info?.name}
-                    <button className="float-right py-0.5 px-2 mr-2 rounded-lg text-[#009ac9] hover:underline">
+                    <button
+                      className="float-right py-0.5 px-2 mr-2 rounded-lg text-[#009ac9] hover:underline"
+                      onClick={handleSubmit}
+                    >
                       Update
                     </button>
                   </dd>

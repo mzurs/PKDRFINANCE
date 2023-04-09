c581488a3ab146e2c4b3c0026203e984d5d461a7
Muhammad Zohaib <zohaib10092001@gmail.com>
2023-04-09 21:21:07 +0500

    loading spinner added shifted to Cards.tsx

--- components/users/Cards.tsx
+++ components/users/Cards.tsx
@@ -1,14 +1,84 @@
-import React from "react";
+import { useAtom, useAtomValue } from "jotai";
+import React, { useEffect, useState } from "react";
+import { userBalance, userInfoAtom, web3authAtom } from "../../state/jotai";
 
 const Cards = (props: any) => {
+  const info = useAtomValue(userInfoAtom);
+  const [ShowMoney, setShowMoney] = useState<boolean>(true);
+  const [Money, setMoney] = useAtom<number>(userBalance);
+  const [timer, setTimer] = useState(6000);
+  const [web3auth] = useAtom(web3authAtom);
+  const [userInfo, setUserInfo] = useAtom(userInfoAtom);
+
+  const getETHBalance = async () => {
+    const headers = new Headers();
+    headers.append("content-type", "application/json");
+    headers.append(
+      "x-custom-header",
+      JSON.stringify([
+        (userInfo as unknown as any).email,
+        (web3auth as unknown as any).idToken,
+        (userInfo as unknown as any).oAuthIdToken,
+      ])
+    );
+    const response = await fetch("/api/user/query/getUserBalance", {
+      method: "GET",
+      headers: headers,
+    });
+    console.log(
+      "🚀 ~ file: Dashboard.tsx:55 ~ getETHBalance ~ response:",
+      response
+    );
+
+    const data = await response.json();
+    //@ts-ignore
+    setMoney(data.getETHBalance);
+    console.log(
+      "🚀 ~ file: Dashboard.tsx:47 ~ getETHBalance ~ data:",
+      data
+    );
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      // your effect code here
+      getETHBalance();
+      // getUsersCount();
+      console.log(timer);
+    }, timer);
+
+    // cleanup function to clear the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, []);
   return (
+
     <div>
       <div className="h-[54vh] m-2 border-2 shadow-lg rounded-lg">
         <div className="px-8 py-4 font-bold">
           <div className="text-2xl pb-3">{props.title}</div>
           <div className="text-lg pl-9">{props.sub}</div>
         </div>
-        <div className="text-[7rem] font-medium flex items-center justify-center h-[30vh]">{props.money}</div>
+       {Money? <div className="text-[4rem] font-medium flex items-center justify-center h-[30vh]">{Money} PKDR</div>: (
+              <div role="status">
+                <svg
+                  aria-hidden="true"
+                  className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+                <span className="sr-only">Loading...</span>
+              </div>
+            )}
         <div>
           <button className="mt-12 bg-blue-700 text-blue-100 text-lg font-medium mb-2 mx-6 px-3 py-2 rounded-xl float-right">
             {props.btn_txt}

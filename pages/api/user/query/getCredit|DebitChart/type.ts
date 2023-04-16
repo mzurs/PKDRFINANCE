a0cf47a0a81465d7d25eddd4ee16f7d548d02e93
Muhammad Zohaib <zohaib10092001@gmail.com>
2023-04-17 02:21:38 +0500

    credit and debit list for chart data api added

---
+++ pages/api/user/query/getCredit|DebitChart/type.ts
@@ -0,0 +1,11 @@
+type DebitReturnParams = {
+    Amount: number;
+    TimeStamp: number;
+  };
+  
+  type CreditReturnParams = {
+    Amount: number;
+    TimeStamp: number;
+  };
+
+  export type {CreditReturnParams,DebitReturnParams}

fe2fa23243bae65986b58339372ea232ed9af440
Muhammad Zohaib <zohaib10092001@gmail.com>
2023-04-17 01:07:17 +0500

    conversion apis and checkout table insertion integrated

--- pages/api/checkout/[id].ts
+++ pages/api/checkout/[id].ts
@@ -11,18 +11,23 @@ import getRateUSDPKR from "../get_USD_PKDR_rate";
 import { mintResult } from "../../../src/API";
 import mintPkdr from "../mutation/mintPKDR";
 import Stripe from "stripe";
+import { CreditParams } from "../user/mutation/transferFrom/types";
+import add_To_Credit_Table from "../user/mutation/transferFrom/addToCreditTable";
+import conversion from "./convertToPKR";
 
 type ReturnResult = {
   result: boolean;
   message: string;
   ETH_ADDRESS?: string;
+  userName: string;
 };
 
 const getUserInfo = async (id: string): Promise<ReturnResult> => {
-  console.log("🚀 ~ file: [id].ts:28 ~ getUserInfo ~ id:", id);
+  // console.log("🚀 ~ file: [id].ts:28 ~ getUserInfo ~ id:", id);
   const res: ReturnResult = {
     result: false,
     message: "",
+    userName: "",
   };
   const input = {
     Key: marshall({
@@ -43,6 +48,7 @@ const getUserInfo = async (id: string): Promise<ReturnResult> => {
   res.result = true;
   res.message = "user found";
   res.ETH_ADDRESS = item["ETH_ADDRESS"];
+  res.userName = item["USERNAME"];
   return res;
 };
 
@@ -50,11 +56,13 @@ const checkAndUpdateCheckoutTable = async (
   sessionId: string,
   amount_received: number,
   authTokens: string[],
-  address: string
+  address: string,
+  userName: string
 ): Promise<ReturnResult> => {
   const res: ReturnResult = {
     message: "",
     result: false,
+    userName: "",
   };
 
   const input = {
@@ -76,13 +84,15 @@ const checkAndUpdateCheckoutTable = async (
 
     const amount = floatNum * rate;
 
+    const timeStamp = Date.now();
+
     const input = {
       TableName: CHECKOUT_TABLE,
       Item: marshall({
         id: sessionId,
         isClaimed: true,
         amount: amount,
-        TimeStamp: Date.now(),
+        TimeStamp: timeStamp,
       }),
     };
     const command = new PutItemCommand(input);
@@ -92,12 +102,31 @@ const checkAndUpdateCheckoutTable = async (
       address,
       String(amount)
     );
-    console.log("🚀 ~ file: [id].ts:100 ~ mintRes:", mintRes);
+    // console.log("🚀 ~ file: [id].ts:100 ~ mintRes:", mintRes);
     res.message = mintRes.message!;
     res.result = mintRes.result ? true : false;
+
+    //----------------------------------------conversion of Amount-------------------------------
+
+    await conversion(Number(amount_received));
+    // const num: number = amount_received;
+    // const floatNum: number = parseFloat((num / 100).toFixed(2));
+
+    // const rate = await getRateUSDPKR();
+
+    // const amount = floatNum * rate;
+    const creditParams: CreditParams = {
+      id: userName,
+      TimeStamp: timeStamp,
+      From: "PKDR_FINANCE",
+      Amount: amount,
+    };
+    // console.log("🚀 ~ file: [id].ts:156 ~ creditParams:", creditParams);
+
+    await add_To_Credit_Table(creditParams);
   } else {
     res.message = "session already claimed";
-    console.log("🚀 ~ file: [id].ts:105 ~ res:", res);
+    // console.log("🚀 ~ file: [id].ts:105 ~ res:", res);
     res.result = false;
     return res;
   }
@@ -112,7 +141,7 @@ const stripe = new Stripe(String(process.env.STRIPE_SECRET_KEY), {
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: any, res: any) => {
   const authTokens = JSON.parse(req.headers["x-custom-header"]);
-  console.log("🚀 ~ file: [id].ts:156 ~ authTokens:", authTokens);
+  // console.log("🚀 ~ file: [id].ts:156 ~ authTokens:", authTokens);
   const resUserInfo: ReturnResult = await getUserInfo(authTokens[0]);
 
   const { id } = req.query;
@@ -120,7 +149,7 @@ export default async (req: any, res: any) => {
   const session = await stripe.checkout.sessions.retrieve(id, {
     expand: ["payment_intent"],
   });
-  console.log("🚀 ~ file: [id].ts:157 ~ session:", session);
+  // console.log("🚀 ~ file: [id].ts:157 ~ session:", session);
 
   const { payment_intent } = session;
 
@@ -132,8 +161,24 @@ export default async (req: any, res: any) => {
       id,
       amount_received,
       authTokens,
-      resUserInfo.ETH_ADDRESS!
+      resUserInfo.ETH_ADDRESS!,
+      resUserInfo.userName
     );
+    if (resCheckoutTable.result) {
+      // await conversion(Number(amount_received));
+      // const num: number = amount_received;
+      // const floatNum: number = parseFloat((num / 100).toFixed(2));
+      // const rate = await getRateUSDPKR();
+      // const amount = floatNum * rate;
+      // const creditParams: CreditParams = {
+      //   id: resUserInfo.userName,
+      //   TimeStamp: Date.now(),
+      //   From: "PKDR_FINANCE",
+      //   Amount: amount,
+      // };
+      // // console.log("🚀 ~ file: [id].ts:156 ~ creditParams:", creditParams);
+      // await add_To_Credit_Table(creditParams);
+    }
     res.status(200).json({ resCheckoutTable, resUserInfo });
   } else {
     const message = "Payment Failure";

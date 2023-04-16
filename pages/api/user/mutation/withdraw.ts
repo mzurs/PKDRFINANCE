f82d0bbe9342ebece194368b3ed7278becb6280b
Muhammad Zohaib <zohaib10092001@gmail.com>
2023-04-17 01:08:19 +0500

    credit and debit apis intergrated with withdraw api

--- pages/api/user/mutation/withdraw.ts
+++ pages/api/user/mutation/withdraw.ts
@@ -1,11 +1,16 @@
 //IBAN PK36SCBL0000001123456702
 import { API, Amplify } from "aws-amplify";
 import awsExports from "../../../../src/aws-exports";
-import { WithdrawMutationVariables, WithdrawParams, WithdrawParamsResult } from "../../../../src/API";
+import {
+  WithdrawMutationVariables,
+  WithdrawParams,
+  WithdrawParamsResult,
+} from "../../../../src/API";
 import { withdraw as withdraw_amount } from "../../../../src/graphql/mutations";
-import { NextApiRequest, NextApiResponse } from "next";
 import { getUserInfo } from "../mutation/addContacts";
 import getAddressFromUserName from "./transferFrom/getAddressByUserName";
+import { DebitParams } from "./transferFrom/types";
+import add_To_Debit_Table from "./transferFrom/addToDebitTable";
 
 Amplify.configure(awsExports);
 
@@ -13,11 +18,10 @@ const withdraw = async function (
   authTokens: string[],
   WithdrawParams: WithdrawParams
 ): Promise<WithdrawParamsResult | any> {
-  
-  console.log("🚀 ~ file: withdraw.ts:26 ~ WithdrawParams:", WithdrawParams)
+  // console.log("🚀 ~ file: withdraw.ts:26 ~ WithdrawParams:", WithdrawParams)
   const authToken = "abc";
 
-  const variables:WithdrawMutationVariables = {
+  const variables: WithdrawMutationVariables = {
     withdrawParams: WithdrawParams,
   };
   try {
@@ -26,7 +30,20 @@ const withdraw = async function (
       variables,
       authToken,
     })) as { data: WithdrawParamsResult };
-    console.log("🚀 ~ file: withdraw.ts:27 ~ res:", res.data);
+    // console.log("🚀 ~ file: withdraw.ts:29 ~ res:", res.data)
+    //@ts-ignore:
+    // console.log("🚀 ~ file: withdraw.ts:27 ~ res:", res.data!.withdraw.result!);
+    if (res.data!.withdraw.result!) {
+      const timeStamp: number = Date.now();
+      const debitParams: DebitParams = {
+        id: WithdrawParams.userName!,
+        TimeStamp: timeStamp,
+        To: "PKDR_FINANCE",
+        Amount: Number(WithdrawParams.amount),
+      };
+
+      await add_To_Debit_Table(debitParams);
+    }
 
     return res.data;
   } catch (error) {
@@ -36,13 +53,12 @@ const withdraw = async function (
 
 export default async function handler(req: any, res: any) {
   if (req.method === "POST") {
-
     const authTokens = JSON.parse(req.headers["x-custom-header"]);
     const { email } = await getUserInfo(authTokens[0]);
     let body = req.body;
     const eth_address = await getAddressFromUserName(authTokens, body.username);
-    
-    console.log("🚀 ~ file: withdraw.ts:46 ~ handler ~ body:", body);
+
+    // console.log("🚀 ~ file: withdraw.ts:46 ~ handler ~ body:", body);
 
     let data: WithdrawParams = {
       IBAN: body.iban,
